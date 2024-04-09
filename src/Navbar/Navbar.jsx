d91313527069d53675d00e769266623d01e8b1e0
aminul157246 <aminul.islam.157246@gmail.com>
2024-04-09 23:55:57 +0600

    added favicon and logo

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,23 +1,34 @@
 import { FaFacebookF, FaGithub, FaLinkedinIn } from "react-icons/fa";
 
-
+import logo from '../../src/assets/default.png';
 
 
 
 const Navbar = () => {
 
+    const navLinks = <>
+
+        <li><a onClick={setActive} href="#home">HOME</a></li>
+        <li><a onClick={setActive} href="#about">ABOUT</a></li>
+        <li><a onClick={setActive} href="#skill">MY SKILL</a></li>
+        <li><a onClick={setActive} href="#experience">MY EXPERIENCE</a></li>
+        <li><a onClick={setActive} href="#project">MY PROJECTS</a></li>
+        <li><a onClick={setActive} href="#contact">CONTACT</a></li>
+
+    </>
+
     function setActive(event) {
         const navLinks = document.querySelectorAll('.menu a');
         navLinks.forEach((navLink) => {
-          navLink.classList.remove('active');
+            navLink.classList.remove('active');
         });
         event.target.classList.add('active');
-      }
+    }
 
-    
+
 
     return (
-        <div className="navbar bg-base-200 fixed px-12">
+        <div className="navbar bg-base-200 fixed pl-32 pr-8 ">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -25,22 +36,17 @@ const Navbar = () => {
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
 
-                    <li><a onClick={setActive} href="#home">HOME</a></li>
-                    <li><a onClick={setActive} href="#about">ABOUT</a></li>
-                    <li><a onClick={setActive} href="#skill">MY SKILL</a></li>
-                    <li><a onClick={setActive} href="#experience">MY EXPERIENCE</a></li>
-                    <li><a onClick={setActive} href="#project">MY PROJECTS</a></li>
-                    <li><a onClick={setActive} href="#contact">CONTACT</a></li>
+                        {navLinks}
 
                     </ul>
                 </div>
-                <a href="#home" className="btn btn-ghost text-xl">AminuL</a>
+                <a href="#home" className="btn btn-ghost text-xl"><img className="w-72 absolute" src={logo} alt="" /></a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
 
 
-                   
+
                     <li><a onClick={setActive} href="#home">HOME</a></li>
                     <li><a onClick={setActive} href="#about">ABOUT</a></li>
                     <li><a onClick={setActive} href="#skill">MY SKILL</a></li>
@@ -53,17 +59,17 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-            <div className="flex gap-2   items-center">
+                <div className="flex gap-2   items-center">
 
-<a href="https://github.com/aminul157246">
-    <FaGithub className="text-2xl hover:text-indigo-500"></FaGithub>
-</a>
-<a href="https://www.linkedin.com/in/aminulislam157246/">
-    <FaLinkedinIn className="text-2xl hover:text-indigo-500"></FaLinkedinIn>
-</a>
-<a href="https://web.facebook.com/profile.php?id=100049235346943"> <FaFacebookF className="text-2xl hover:text-indigo-500"></FaFacebookF>
-</a>
-</div>
+                    <a href="https://github.com/aminul157246">
+                        <FaGithub className="text-2xl hover:text-indigo-500"></FaGithub>
+                    </a>
+                    <a href="https://www.linkedin.com/in/aminulislam157246/">
+                        <FaLinkedinIn className="text-2xl hover:text-indigo-500"></FaLinkedinIn>
+                    </a>
+                    <a href="https://web.facebook.com/profile.php?id=100049235346943"> <FaFacebookF className="text-2xl hover:text-indigo-500"></FaFacebookF>
+                    </a>
+                </div>
             </div>
         </div>
     );
