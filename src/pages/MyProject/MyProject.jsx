b14e9b3e24f4de13850be8bcd73ed0dd1a7b2f92
aminul157246 <aminul.islam.157246@gmail.com>
2024-04-10 19:06:59 +0600

    updated project

--- src/pages/MyProject/MyProject.jsx
+++ src/pages/MyProject/MyProject.jsx
@@ -1,7 +1,23 @@
-import img1 from '../../assets/home.png'
-import img2 from '../../assets/22project.png'
-import img3 from '../../assets/bridly.png'
+
 import { FaChevronRight } from "react-icons/fa";
+
+import img1 from '../../assets/banner.png'
+import img2 from '../../assets/apartment.png'
+import img3 from '../../assets/perpect.png'
+import img4 from '../../assets/mission.png'
+import img5 from '../../assets/blog.png'
+import img6 from '../../assets/agree.png'
+import img7 from '../../assets/event-banner.png'
+import img8 from '../../assets/dream.png'
+import img9 from '../../assets/event.png'
+import img10 from '../../assets/team.png'
+import img11 from '../../assets/contact.png'
+import img12 from '../../assets/product.png'
+import img13 from '../../assets/what.png'
+import img14 from '../../assets/car.png'
+import img15 from '../../assets/car-banner.png'
+import img16 from '../../assets/test.png'
+import img17 from '../../assets/loan.png'
 // import './MyProject.css'
 
 
@@ -23,113 +39,137 @@ const MyProject = () => {
             <div className='grid grid-cols-1 lg:grid-cols-3 gap-12 justify-center items-center'>
 
                 <div>
-                    
+                    <div className="shadow-xl p-8">
+                        <div className="single-team">
+                            <Swiper
+                                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                                spaceBetween={50}
+                                slidesPerView={1}
+                                navigation
+                                pagination={{ clickable: true }}
+                                scrollbar={{ draggable: true }}
+                                onSwiper={(swiper) => console.log(swiper)}
+                                onSlideChange={() => console.log('slide change')}
+                            >
+                                <SwiperSlide><img className=' h-80' src={img1} alt="" /></SwiperSlide>
+                                <SwiperSlide><img className=' h-80' src={img2} alt="" /></SwiperSlide>
+                                <SwiperSlide><img className=' h-80' src={img3} alt="" /></SwiperSlide>
+                                <SwiperSlide><img className=' h-80' src={img4} alt="" /></SwiperSlide>
+                                <SwiperSlide><img className=' h-80' src={img5} alt="" /></SwiperSlide>
+                                <SwiperSlide><img className=' h-80' src={img6} alt="" /></SwiperSlide>
 
+                            </Swiper>
 
-
-                        <div className="shadow-xl p-8">
-                            <div className="single-team">
-
-                                {/* <figure><img className=" rounded-lg" src={img1} alt="Shoes" /></figure> */}
-
-                                <Swiper
-      // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-      slidesPerView={1}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-    >
-      <SwiperSlide><img className=' h-80'  src={img1} alt=""/></SwiperSlide>
-      <SwiperSlide><img className=' h-80' src={img1} alt=""/>2</SwiperSlide>
-      <SwiperSlide><img className=' h-80' src={img1} alt=""/>3</SwiperSlide>
-      <SwiperSlide><img className=' h-80' src={img1} alt=""/>4</SwiperSlide>
-      ...
-    </Swiper>
-
-
-
-
-
-
-                                <div className=" flex justify-between ">
-                                    <a href="https://remarkable-babka-8ef47b.netlify.app/">
-                                    <div className="card-actions  mt-16 flex items-center">
+                            <div className=" flex justify-between ">
+                                <a href="https://remarkable-babka-8ef47b.netlify.app/">
+                                    <div className="card-actions  mt-16 flex items-center transition-transform transform hover:scale-110 transition-delay-300">
                                         <p>see project</p>
                                         <FaChevronRight className='text-xl'></FaChevronRight>
                                     </div>
-                                    </a>
+                                </a>
 
-                                    <a href="https://remarkable-babka-8ef47b.netlify.app/">
-                                    <div className="card-actions  mt-16 flex items-center">
+                                <a href="https://github.com/aminul157246/apartment-booking-client">
+                                    <div className="card-actions  mt-16 flex items-center transition-transform transform hover:scale-110 transition-delay-300">
                                         <p>Github</p>
                                         <FaChevronRight className='text-xl'></FaChevronRight>
                                     </div>
-                                    </a>
-                                </div>
+                                </a>
                             </div>
                         </div>
-                  
-
+                    </div>
                 </div>
 
-                <div className='lg:mt-36'>
-                    <a href="https://elegant-marzipan-f0ca7b.netlify.app/ ">
 
 
-                        <div className=" shadow-xl">
-                            <div className="single-team">
 
 
-                                <figure><img className='h-[250px] w-[250px] rounded-lg' src={img3} alt="Shoes" /></figure>
-                                <div className="team-text">
-                                    <h2 className="text-2xl font-bold">Bridely</h2>
 
-                                    <div className="card-actions justify-end mt-16">
+
+                <div>
+                    <div className="shadow-xl p-8">
+                        <div className="single-team">
+                            <Swiper
+                                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                                spaceBetween={50}
+                                slidesPerView={1}
+                                navigation
+                                pagination={{ clickable: true }}
+                                scrollbar={{ draggable: true }}
+                                onSwiper={(swiper) => console.log(swiper)}
+                                onSlideChange={() => console.log('slide change')}
+                            >
+                                <SwiperSlide><img className=' h-80' src={img7} alt="" /></SwiperSlide>
+                                <SwiperSlide><img className=' h-80' src={img8} alt="" /></SwiperSlide>
+                                <SwiperSlide><img className=' h-80' src={img9} alt="" /></SwiperSlide>
+                                <SwiperSlide><img className=' h-80' src={img10} alt="" /></SwiperSlide>
+                                <SwiperSlide><img className=' h-80' src={img11} alt="" /></SwiperSlide>
+                                <SwiperSlide><img className=' h-80' src={img12} alt="" /></SwiperSlide>
+                                <SwiperSlide><img className=' h-80' src={img13} alt="" /></SwiperSlide>
+
+                            </Swiper>
+
+                            <div className=" flex justify-between ">
+                                <a href="https://elegant-marzipan-f0ca7b.netlify.app/ ">
+                                    <div className="card-actions  mt-16 flex items-center transition-transform transform hover:scale-110 transition-delay-300">
                                         <p>see project</p>
-                                        <FaChevronRight className='text-3xl' ></FaChevronRight >
-
+                                        <FaChevronRight className='text-xl'></FaChevronRight>
                                     </div>
-                                </div>
+                                </a>
+
+                                <a href="https://github.com/aminul157246/event-management">
+                                    <div className="card-actions  mt-16 flex items-center transition-transform transform hover:scale-110 transition-delay-300">
+                                        <p>Github</p>
+                                        <FaChevronRight className='text-xl'></FaChevronRight>
+                                    </div>
+                                </a>
                             </div>
                         </div>
-
-
-                    </a>
-
-
-
+                    </div>
                 </div>
 
-                <div className='lg:mt-72'>
-                    <a href="https://brand-shop-d10ed.web.app/">
 
 
-                        <div className=" shadow-xl">
-                            <div className="single-team">
 
 
-                                <figure><img className='h-[250px] w-[250px] rounded-lg' src={img2} alt="Shoes" /></figure>
-                                <div className="team-text">
-                                    <h2 className="text-2xl font-bold">Car Shop</h2>
 
-                                    <div className="card-actions justify-end mt-16">
+                <div>
+                    <div className="shadow-xl p-8">
+                        <div className="single-team">
+                            <Swiper
+                                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                                spaceBetween={50}
+                                slidesPerView={1}
+                                navigation
+                                pagination={{ clickable: true }}
+                                scrollbar={{ draggable: true }}
+                                onSwiper={(swiper) => console.log(swiper)}
+                                onSlideChange={() => console.log('slide change')}
+                            >
+                                <SwiperSlide><img className=' h-80' src={img14} alt="" /></SwiperSlide>
+                                <SwiperSlide><img className=' h-80' src={img15} alt="" /></SwiperSlide>
+                                <SwiperSlide><img className=' h-80' src={img16} alt="" /></SwiperSlide>
+                                <SwiperSlide><img className=' h-80' src={img17} alt="" /></SwiperSlide>
+
+                            </Swiper>
+
+                            <div className=" flex justify-between ">
+                                <a href="https://brand-shop-d10ed.web.app/">
+                                    <div className="card-actions  mt-16 flex items-center transition-transform transform hover:scale-110 transition-delay-300">
                                         <p>see project</p>
-                                        <FaChevronRight className='text-3xl'></FaChevronRight>
-
+                                        <FaChevronRight className='text-xl'></FaChevronRight>
                                     </div>
-                                </div>
+                                </a>
+
+                                <a href="https://github.com/aminul157246/brandshop-client">
+                                    <div className="card-actions  mt-16 flex items-center transition-transform transform hover:scale-110 transition-delay-300">
+                                        <p>Github</p>
+                                        <FaChevronRight className='text-xl'></FaChevronRight>
+                                    </div>
+                                </a>
                             </div>
                         </div>
-
-
-                    </a>
-
+                    </div>
                 </div>
-
             </div>
 
         </div>
